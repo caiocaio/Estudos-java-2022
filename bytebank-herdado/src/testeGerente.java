f1f7d5187122b7dfc0029ed7e94c80973146b658
Caio Marquez Chaves <30413174+caiocaio@users.noreply.github.com>
2022-11-12 23:38:05 -0300

    poli-12-11-22

---
+++ bytebank-herdado/src/testeGerente.java
@@ -0,0 +1,18 @@
+public class testeGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("Caio Marquez Chaves");
+        g1.setCpf("826361672646");
+        g1.setSalario(3000.0);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+
+        g1.setSenha(2222);
+        boolean autenticou = g1.autentica(2222);
+
+
+        System.out.println(autenticou);
+    }
+}
