310b7bbb4a1f517f3d7756ad882cef739254f4fb
Caio Marquez Chaves <30413174+caiocaio@users.noreply.github.com>
2022-11-22 20:12:29 -0300

    excep-22-11-22

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -10,22 +10,21 @@ public class Fluxo {
             System.out.println("Exception " + msg);
             ex.printStackTrace();
         }
-        System.out.println("Fim do metodo1");
+
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
             metodo2();
+        System.out.println("Fim do metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-                int a = i /0;
-            //Conta c = null;
-           // c.deposita();
-        }
-        System.out.println("Fim do metodo2");
+
+        throw new ArithmeticException("Deu errado! ");
+
+
+       // System.out.println("Fim do metodo2");
     }
 }
