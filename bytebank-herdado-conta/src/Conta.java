bd25f00bf4626a3ddc96b6991802b7ebe292248b
caiocaio <caiomchaves@gmail.com>
2022-11-28 23:49:31 -0300

    excep-28-11-22

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -21,23 +21,21 @@ public abstract class Conta {
 
     public abstract void deposita(double valor);
 
-    public boolean saca(double valor){
-        if(this.saldo >= valor){
-            this.saldo -=  valor;
-            return true;
-        }else{
-            return false;
+    public void saca(double valor) throws SaldoInsuficienteException {
+        if(this.saldo < valor){
+           //problema
+            throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", " + "Valor: "+ valor);
+
         }
+
+        this.saldo -=  valor;
     }
 
-    public boolean transfere(double valor, Conta destino){
-        if(this.saca(valor)){
-            destino.deposita(valor);   //pode ser destino.saldo += valor;
-            return true;
-        } //else{} opicional aqui, por uma questão logica.
-            return false;
-
+    public void transfere(double valor, Conta destino) throws SaldoInsuficienteException {
+        this.saca(valor);
+        destino.deposita(valor);
     }
+
 
     public double getSaldo(){
         return this.saldo;
--- java-pilha/src/FluxoComErro.java
+++ java-pilha/src/FluxoComErro.java
@@ -4,7 +4,7 @@ public class FluxoComErro {
         System.out.println("Ini do main");
         try {
         metodo1();
-        }catch (ArithmeticException | NullPointerException | MinhaExcecao ex){
+        }catch (ArithmeticException | NullPointerException  ex){
             String  msg = ex.getMessage();
             System.out.println("Exception " + msg);
             ex.printStackTrace();
@@ -12,13 +12,13 @@ public class FluxoComErro {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao{
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
             metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao{
+    private static void metodo2() {
         System.out.println("Ini do metodo2");
         metodo2();
         System.out.println("Fim do metodo2");
