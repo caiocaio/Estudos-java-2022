29c248412631f86d8bd900d656b0554ad9dd4887
caiocaio <caiomchaves@gmail.com>
2022-11-30 22:51:49 -0300

    excep-Final-30-11-22

--- java-pilha/src/TestaConexao.java
+++ java-pilha/src/TestaConexao.java
@@ -1,15 +1,27 @@
-public class TestaConexao {
+public class TestaConexao  {
     public static void main(String[] args) {
 
-        Conexao con = null;
-        try {
-            con = new Conexao();
-            con.leDados();
-        }catch (IllegalStateException ex){
-            System.out.println("Deu Erro na Conexao");
-        }finally {
-            con.fecha();
-        }
+       try(Conexao conexao = new Conexao()) {
+           conexao.leDados();
+       }catch(IllegalStateException ex){
+           System.out.println("Deu Erro na Conexao");
+       }
 
-    }
+
+
+       //--------------------------------
+//        Conexao con = null;
+//        try {
+//            con = new Conexao();
+//            con.leDados();
+//        }catch (IllegalStateException ex){
+//            System.out.println("Deu Erro na Conexao");
+//        }finally {
+//            if(con != null) {
+//                con.close();
+//            }
+//        }
+//
+//
+     }
 }
